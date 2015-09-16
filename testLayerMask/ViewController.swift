c3db5da346148b9c7f802ce112674d7818ff4a09
csjlengxiang <csjlengxiang@gmail.com>
2015-09-16 17:10:11 +0800

    runkeeperswitch_principle
    https://github.com/gontovnik/DGRunkeeperSwitch
    这个的视线原理

--- testLayerMask/ViewController.swift
+++ testLayerMask/ViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var lb: UILabel!
+    var ct: UIView!
+    var ml: CALayer!
+    
+    @IBAction func clicked(sender: AnyObject) {
+        ml.frame = CGRectMake(ml.frame.origin.x + 1, 0, 50, 50)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lb = UILabel()
+        lb.frame = CGRectMake(0, 10, 100, 30)
+        lb.text = "你好哇"
+        lb.textColor = UIColor.blackColor()
+        lb.textAlignment = NSTextAlignment.Center
+        ct = UIView()
+        ct.backgroundColor = UIColor.grayColor()
+        ct.addSubview(lb)
+        ct.frame = CGRectMake(0, 20, 100, 50)
+        self.view.addSubview(ct)
+        
+        ml = CAShapeLayer()
+        ml.frame = CGRectMake(0, 0, 50, 50)
+        ml.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).CGColor
+        ct.layer.mask = ml
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
